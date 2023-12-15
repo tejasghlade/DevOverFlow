d7d51124b43bd744d81b04db774dce848fd252d9
tejasghlade <tejasghlade@gmail.com>
2023-12-15 21:20:28 +0530

    routing

---
+++ app/(root)/(home)/ask-question/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Ask Questions</div>
+  )
+}
+
+export default page
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Next js We Are Comming</h1>
-    </div>
-  );
-}
